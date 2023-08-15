32f7480a3cdc0e8700c3231268e6e244254cef03
Yash Pratap Singh <yash.singh@olx.com>
2023-08-16 04:12:35 +0530

    Add launcher icons

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Desi",
       theme: _buildTheme(Brightness.dark),
       home: HomeScreen(),
